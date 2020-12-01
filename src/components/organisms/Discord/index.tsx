56d8316b191a6003c3974a89602b2d6fdb6cbe8e
kohei_kawamura <piro.haniwa@gmail.com>
2020-12-01 23:42:07 +0900

    feat: add discord

---
+++ src/components/organisms/Discord/index.tsx
@@ -0,0 +1,17 @@
+import WidgetBot from '@widgetbot/react-embed';
+import React, { FC } from 'react';
+
+const DiscordWidget: FC = () => (
+  <WidgetBot
+    channel="783321509477220394"
+    server="783321508918460427"
+    style={{
+      height: '100%',
+      maxHeight: '450px',
+      maxWidth: '800px',
+      width: '100%',
+    }}
+  />
+);
+
+export default DiscordWidget;
