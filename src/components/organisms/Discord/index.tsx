48c9a374f1284c7bb38c08c2a9471da7601851f9
piro <piro.haniwa@gmail.com>
2020-12-02 00:14:37 +0900

    fix: set false to defer on discord widget

--- src/components/organisms/Discord/index.tsx
+++ src/components/organisms/Discord/index.tsx
@@ -4,6 +4,7 @@ import React, { FC } from 'react';
 const DiscordWidget: FC = () => (
   <WidgetBot
     channel="783321509477220394"
+    defer={false}
     server="783321508918460427"
     style={{
       height: '100%',
