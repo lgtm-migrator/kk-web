a77a70c6d0065b370e527a7f2355df991c89a660
piro <piro.haniwa@gmail.com>
2021-04-13 13:24:59 +0900

    feat: add blog

--- src/components/templates/Seo/index.tsx
+++ src/components/templates/Seo/index.tsx
@@ -21,7 +21,7 @@ const Seo: FC<SeoProps> = ({
   const {
     file: { publicURL },
     site: {
-      siteMetadata: { description, homepage, name },
+      siteMetadata: { description, name },
     },
   } = useStaticQuery(graphql`
     query SeoQuery {
@@ -31,7 +31,6 @@ const Seo: FC<SeoProps> = ({
       site {
         siteMetadata {
           description
-          homepage
           name
         }
       }
@@ -43,7 +42,7 @@ const Seo: FC<SeoProps> = ({
       <html lang="ja" />
       <meta content="438978880268835" property="fb:app_id" />
       <meta content={ogDescription || description} property="og:description" />
-      <meta content={`${homepage}${publicURL}`} property="og:image" />
+      <meta content={`https://kk-web.link"${publicURL}`} property="og:image" />
       <meta content="630" property="og:image:height" />
       <meta content="1200" property="og:image:width" />
       <meta content="ja_JP" property="og:locale" />
@@ -56,7 +55,7 @@ const Seo: FC<SeoProps> = ({
         property="og:title"
       />
       <meta content={ogType} property="og:type" />
-      <meta content={`${homepage}${path}`} property="og:url" />
+      <meta content={`https://kk-web.link"${path}`} property="og:url" />
       <meta content="piro" name="author" />
       <meta content={ogDescription || description} name="description" />
       <meta content="summary_large_image" name="twitter:card" />
